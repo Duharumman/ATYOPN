04f3db4c4ad269f19f1b328356b97d76b8377f3c
Duha <duha.abu99@gmail.com>
2021-01-08 15:09:18 +0200

    test

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Master");
         System.out.println("new branch");
+        System.out.println("test ");
     }
 }
